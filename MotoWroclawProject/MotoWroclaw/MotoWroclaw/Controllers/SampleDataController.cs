7a1cbd98be2b5a03c553a7af5920e1d7c169ae3e
Robert <robsonlanser@gmail.com>
2019-10-01 21:12:23 +0200

    Added Windy weather, testing

--- MotoWroclawProject/MotoWroclaw/MotoWroclaw/Controllers/SampleDataController.cs
+++ MotoWroclawProject/MotoWroclaw/MotoWroclaw/Controllers/SampleDataController.cs
@@ -11,7 +11,7 @@ namespace MotoWroclaw.Controllers
     {
         private static string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Windy"
         };
 
         [HttpGet("[action]")]
